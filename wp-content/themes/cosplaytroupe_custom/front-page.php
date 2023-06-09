c486577cd741afdf08b080fe2d36d87097f799bb
Evan Stoddard (RIT Student) <eds2083@rit.edu>
2023-06-09 16:23:36 -0400

    Custom Fields

--- wp-content/themes/cosplaytroupe_custom/front-page.php
+++ wp-content/themes/cosplaytroupe_custom/front-page.php
@@ -1,10 +1,26 @@
 <?php get_header() ?>
 
-<div class="advert-banner">
-    <h1>PLAIN TEXT INPUT HERE</h1>
-    <p>RICH TEXT INPUT HERE</p>
-</div>
+<?php if (get_field("banner_show")) { ?>
+    <header class="advert-banner" style="background-image: <?php echo get_field("banner_background-image"); ?>">
+        <h1><?php echo get_field("banner_heading"); ?></h1>
+        <p><?php echo get_field("banner_body"); ?></p>
+    </header>
+<?php } ?>
 
 <main>
-    
+    <div class="recent-posts">
+        <?php 
+            $homepagePostList = new WP_Query(array(
+                'posts_per_page' => 5,
+                'post_type' => 'post'
+            ));
+            while($homepagePostList->have_posts()) {
+                $homepagePostList->the_post();
+        ?>
+        
+        <?php } ?>
+    </div>
+    <div class="important-links">
+
+    </div>
 </main>
