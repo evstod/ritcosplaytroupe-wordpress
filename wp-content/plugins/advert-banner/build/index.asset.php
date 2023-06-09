0a6e811c54523980aff3f80205cae58750cb1c35
Evan Stoddard (RIT Student) <eds2083@rit.edu>
2023-06-09 14:14:54 -0400

    Banner Block

---
+++ wp-content/plugins/advert-banner/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd28566da9357547727f4');
--- wp-content/themes/cosplaytroupe_custom/front-page.php
+++ wp-content/themes/cosplaytroupe_custom/front-page.php
@@ -1,6 +1,9 @@
 <?php get_header() ?>
 
-
+<div class="advert-banner">
+    <h1>PLAIN TEXT INPUT HERE</h1>
+    <p>RICH TEXT INPUT HERE</p>
+</div>
 
 <main>
     
